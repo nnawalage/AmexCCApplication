94b8c7dd17b03c6bf1399663b9624e8250489a00
nnawalage <dhano.nn@gmail.com>
2017-08-29 14:17:14 +0530

    Project Setup

---
+++ Amex.CCA.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Amex.CCA.WebApi.WebApiApplication" Language="C#" %>
---
+++ Amex.CCA.WebClient/app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './routes';
+import{NavBarComponent,DashboardComponent} from './index';
+
+@NgModule({
+  declarations: [
+    AppComponent, NavBarComponent, DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
