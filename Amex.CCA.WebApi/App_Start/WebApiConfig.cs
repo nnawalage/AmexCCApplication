54dd7eacfb107307495114dbcc07563159472b8e
pmd <pmd@TI-NB-124>
2017-09-06 16:54:59 +0530

    Enable cors changes

--- Amex.CCA.WebApi/App_Start/WebApiConfig.cs
+++ Amex.CCA.WebApi/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace Amex.CCA.WebApi
 {
@@ -16,7 +17,8 @@ namespace Amex.CCA.WebApi
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            EnableCorsAttribute attribute = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(attribute);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
