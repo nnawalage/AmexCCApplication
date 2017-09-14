feb469170c131a235ff9c309bfb9105bdaafe586
amila-suranga <ase@tiqri.com>
2017-09-14 14:46:54 +0530

    Missing Files

---
+++ Amex.CCA.WebClient/app/src/app/services/nationality-resolver.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { CrediCardService } from '../services/index';
+
+@Injectable()
+export class NationalityResolverService implements Resolve<any>{
+    constructor(private creditCardService: CrediCardService) { }
+
+    resolve(router: ActivatedRouteSnapshot) {
+        // return this.creditCardService.getNationalities();
+           this.creditCardService.getNationalities();
+    }
+
+}
