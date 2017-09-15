310a54a18507915888283a81844bd1d3404a7ab5
pmd <pmd@TI-NB-124>
2017-09-15 10:50:39 +0530

    Logs tables added.

---
+++ Amex.CCApplication.DataAccess/Entities/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amex.CCA.DataAccess.Entities
+{
+    public class Log : AmexModelBase
+    {
+        public int LogId { get; set; }
+
+        public string Action { get; set; }
+
+        public string Comment { get; set; }
+
+        public int CreditCardId { get; set; }
+
+        public virtual CreditCard CreditCard { get; set; }
+    }
+}
