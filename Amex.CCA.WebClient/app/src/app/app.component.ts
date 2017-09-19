8bc19ca4309de9c6f32286363b6e10f649ebf8b2
amila-suranga <ase@tiqri.com>
2017-09-19 17:02:12 +0530

    User Profile Dev

--- Amex.CCA.WebClient/app/src/app/app.component.ts
+++ Amex.CCA.WebClient/app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LoginService } from "./services/login.service";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   constructor(private loginService: LoginService) { }
