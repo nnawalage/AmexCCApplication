aab4db6fd4de6b8a551b2b926b51c331d2380326
Chanaka Gamage <chanacse@gmail.com>
2017-09-22 11:51:14 +0530

    user module merged

--- Amex.CCA.WebClient/app/src/app/user/user.module.ts
+++ Amex.CCA.WebClient/app/src/app/user/user.module.ts
@@ -1,11 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { userRoutes } from './user.routes';
-import { EditProfileComponent } from './edit-profile.component';
-
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {userRoutes} from './user.routes';
+import {EditProfileComponent} from './edit-profile.component';
 @NgModule({
     imports: [
         CommonModule,
@@ -14,6 +10,7 @@ import { EditProfileComponent } from './edit-profile.component';
     ],
     declarations: [
         EditProfileComponent,
+        UserRegistrationComponent
     ],
     providers: []
 })
