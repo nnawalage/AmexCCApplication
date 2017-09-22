e5eedce31ace8a1909b5b926f9eef683e68a1166
pmd <pmd@TI-NB-124>
2017-09-22 14:55:35 +0530

    Credi card Modals added

--- Amex.CCA.WebClient/app/src/app/dashboard/dashboard.component.ts
+++ Amex.CCA.WebClient/app/src/app/dashboard/dashboard.component.ts
@@ -2,31 +2,65 @@ import {Component} from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { CrediCardService } from "../services/creditcard.service";
 import { Router } from "@angular/router";
-import { FormsModule } from '@angular/forms';
 import { ICreditCardView } from "../models/creditcardview";
-
+import { ViewCreditCardComponent } from '../creditcard/viewcreditcard.component';
+import { EditCreditCardComponent } from '../creditcard/editcreditcard.component';
+import { ApproveCreditCardComponent } from '../creditcard/approvecreditcard.component';
+import { DialogService } from "ng2-bootstrap-modal";
 
 @Component({
-templateUrl:'./dashboard.template.html',
-styleUrls:['./dashboard.styles.scss']
+    templateUrl: './dashboard.template.html',
+    styleUrls: ['./dashboard.styles.scss']
 })
-export class DashboardComponent{
+export class DashboardComponent {
     cardRequestList: ICreditCardView[];
     cardRequest: ICreditCardView;
- constructor(private actRouter: ActivatedRoute, private router: Router, private crediCardService: CrediCardService) {
- }
+    promptMessage: string = '';
 
- ngOnInit() {
-     this.crediCardService.getAllCardRequests().subscribe(receivedCardRequests => {
-         this.cardRequestList= receivedCardRequests;
-         console.log(this.cardRequestList)
-         }, error => console.log(error));
- }
+    constructor(private actRouter: ActivatedRoute, private router: Router, private crediCardService: CrediCardService, private dialogService: DialogService) {
 
+    }
 
- //clickRequest() {
- //    let expandableStatus: boolean = this.cardRequest.IsExpandable;
- //    this.cardRequest.IsExpandable = !expandableStatus;
-     
- //}
+    ngOnInit() {
+        this.crediCardService.getAllCardRequests().subscribe(receivedCardRequests => {
+            this.cardRequestList = receivedCardRequests;
+            console.log(this.cardRequestList)
+        }, error => console.log(error));
+    }
+
+    openCreditCardView() {
+        this.dialogService.addDialog(ViewCreditCardComponent, {
+            question: 'What is your name?: '
+        })
+            .subscribe((message) => {
+                //We get dialog result
+                this.promptMessage = message;
+            });
+    }
+
+    openCreditCardEdit() {
+        this.dialogService.addDialog(EditCreditCardComponent, {
+            question: 'What is your name?: '
+        })
+            .subscribe((message) => {
+                //We get dialog result
+                this.promptMessage = message;
+            });
+    }
+
+    openCreditCardApprove() {
+        this.dialogService.addDialog(ApproveCreditCardComponent, {
+            question: 'What is your name?: '
+        })
+            .subscribe((message) => {
+                //We get dialog result
+                this.promptMessage = message;
+            });
+    }
+
+    //clickRequest() {
+    //    let expandableStatus: boolean = this.cardRequest.IsExpandable;
+    //    this.cardRequest.IsExpandable = !expandableStatus;
+
+    //}
 }
--- Amex.CCA.WebClient/app/src/app/dashboard/dashboard.module.ts
+++ Amex.CCA.WebClient/app/src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { dashBoardRoutes } from './dashboard.routes';
 import { DashboardComponent } from './dashboard.component';
-import { CommonModule } from '@angular/common'; 
-
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { CreditCardModule } from '../creditcard/creditcard.module';
 
 @NgModule({
-    imports: [CommonModule,
+    imports: [CommonModule, FormsModule,
+        BootstrapModalModule,
+        CreditCardModule,
         RouterModule.forChild(dashBoardRoutes)
     ],
     declarations: [
-         DashboardComponent
+        DashboardComponent
     ],
     providers: [
     ]
