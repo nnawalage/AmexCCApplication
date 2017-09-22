18e2f4b765987a6a13475189a51afcf0bfdc8ca1
Chanaka Gamage <chanacse@gmail.com>
2017-09-22 11:55:21 +0530

    Revert "user module merged"
    This reverts commit aab4db6fd4de6b8a551b2b926b51c331d2380326.

--- Amex.CCA.WebClient/app/src/app/user/user.module.ts
+++ Amex.CCA.WebClient/app/src/app/user/user.module.ts
@@ -1,7 +1,11 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {userRoutes} from './user.routes';
-import {EditProfileComponent} from './edit-profile.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { userRoutes } from './user.routes';
+import { EditProfileComponent } from './edit-profile.component';
+
 @NgModule({
     imports: [
         CommonModule,
@@ -10,7 +14,6 @@ import {EditProfileComponent} from './edit-profile.component';
     ],
     declarations: [
         EditProfileComponent,
-        UserRegistrationComponent
     ],
     providers: []
 })
