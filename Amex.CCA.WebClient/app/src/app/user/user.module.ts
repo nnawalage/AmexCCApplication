44bd587caa8176df9d9809b439e7fbac9b35e3b6
Chanaka Gamage <chanacse@gmail.com>
2017-09-22 11:45:32 +0530

    User profile Edit Page created

--- Amex.CCA.WebClient/app/src/app/user/user.module.ts
+++ Amex.CCA.WebClient/app/src/app/user/user.module.ts
@@ -1,17 +1,22 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {userRoutes} from './user.routes';
-import {EditProfileComponent} from './edit-profile.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { userRoutes } from './user.routes';
+import { EditProfileComponent } from './edit-profile.component';
+
 @NgModule({
-    imports:[
-     RouterModule.forChild(userRoutes)
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(userRoutes)
     ],
-    declarations:[
+    declarations: [
         EditProfileComponent,
     ],
-    providers:[]
+    providers: []
 })
-export class UserModule{
+export class UserModule {
 
 }
-      
