aabf876ed44a2ed86ec3307d0b48c0f6ece5e0dc
Chanaka Gamage <chanacse@gmail.com>
2017-09-26 15:02:57 +0530

    edit userprofile with userprofileid

--- Amex.CCA.WebClient/app/src/app/user/edit-profile.component.ts
+++ Amex.CCA.WebClient/app/src/app/user/edit-profile.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { UserProfileService } from '../services/userprofile.service'
 import { IUserProfile } from "../models/userprofile";
+import { LoginService } from '../services/login.service';
 
 @Component({
     templateUrl: './edit-profile.template.html',
@@ -14,13 +15,13 @@ export class EditProfileComponent implements OnInit {
     //This is to load all the controls
     private profileForm: FormGroup;
 
-    constructor(private actRouter: ActivatedRoute, private router: Router, private upService: UserProfileService, private _fb: FormBuilder) {
+    constructor(private actRouter: ActivatedRoute, private router: Router, private upService: UserProfileService, private _fb: FormBuilder, private loginService: LoginService) {
     }
 
 
     ngOnInit() {
         this.profileForm = this._fb.group({
-            userName: ['pmd@tiqri.com', [Validators.required]],
+            userName: ['cpk@tiqri.com', [Validators.required]],
             profileName: ['profilename1', Validators.required],
             profileImage: ['https://lh3.googleusercontent.com/RmjpzTM-vciDR8-B30N2Zbs3ZvVGrxkJQXirUlPMwGvZhSfmucp6D5V8MfgYNwlIqXY=w300'],
         })
@@ -32,14 +33,14 @@ export class EditProfileComponent implements OnInit {
                 UserName: userProfileFormValues['userName'],
                 ProfileName: userProfileFormValues['profileName'],
                 ProfileImage: userProfileFormValues['profileImage'],
+                userProfileId:null
             }
-            //Collecting USERNAME from session
-           
-
-
-            //userProfileObj.
-            console.log(userProfileObj.ProfileName);
+            //Collecting USERNAME, UserProfileID from session store
+            userProfileObj.UserName = this.loginService.loggedUser.UserName;
+            userProfileObj.userProfileId = this.loginService.loggedUser.RoleId; //TESTING
             
+            console.log(userProfileObj.UserName);
+
             this.upService.SaveUserProfile(userProfileObj).subscribe((res: any) => {
                 console.log(res);
             }, error => {
