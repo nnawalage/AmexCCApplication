b91becc6ad92ae88d2066242c900b1ca9e89532e
pmd <pmd@TI-NB-124>
2017-09-27 12:52:09 +0530

    Modal changes

--- Amex.CCA.WebClient/app/src/app/creditcard/editcreditcard.component.ts
+++ Amex.CCA.WebClient/app/src/app/creditcard/editcreditcard.component.ts
@@ -4,64 +4,36 @@ import { FormsModule } from '@angular/forms';
 import {ICreditCard} from '../models/creditcard';
 import { CrediCardService } from "../services/creditcard.service";
 
+export interface PromptModel {
+    CreditCard: ICreditCard;
+    CreditCardId: number;
+}
+
 @Component({
     selector: 'prompt',
     templateUrl: './editcreditcard.template.html'
 })
-export class EditCreditCardComponent extends DialogComponent<ICreditCard, string> implements ICreditCard {
+export class EditCreditCardComponent extends DialogComponent<PromptModel, string> implements PromptModel {
 
+    CreditCard: any;
     CreditCardId: number;
-    FullName: string ;
-    DisplayName: string;
-    Nic: string;
-    Passport: string;
-    Address: string;
-    MobilePhone: string;
-    HomePhone: string;
-    OfficePhone: string; 
-    Email: string;
-    Employer: string;
-    Salary: number;
-    JobTitle: string;
-    CardLimit: number;
-    CashLimit: number;
-    Note: string;
-    CardTypeId: number;
-    NationalityId: number;
 
     constructor(dialogService: DialogService, private crediCardService: CrediCardService) {
         super(dialogService);
     }
 
     ngOnInit() {
+        this.CreditCard = new Object();
         document.getElementsByTagName('body')[0].classList.add('modal-open');
-
         this.crediCardService.getCardDetails(this.CreditCardId).subscribe((creditCard: ICreditCard) => {
             console.log(creditCard);
-            this.CreditCardId = creditCard.CreditCardId;
-            this.FullName = creditCard.FullName;
-            this.DisplayName = creditCard.DisplayName;
-            this.Nic = creditCard.Nic;
-            this.Passport = creditCard.Passport;
-            this.Address = creditCard.Address;
-            this.MobilePhone = creditCard.MobilePhone;
-            this.HomePhone = creditCard.HomePhone;
-            this.OfficePhone = creditCard.OfficePhone;
-            this.Email = creditCard.Email;
-            this.Employer = creditCard.Employer;
-            this.Salary = creditCard.Salary;
-            this.JobTitle = creditCard.JobTitle;
-            this.CardLimit = creditCard.CardLimit;
-            this.CashLimit = creditCard.CashLimit;
-            this.Note = creditCard.Note;
-            this.CardTypeId = creditCard.CardTypeId;
-            this.NationalityId = creditCard.NationalityId;
-
+            this.CreditCard = creditCard;
         }, error => console.log(error));
     }
 
-    apply() {
+    save() {
         //this.result = this.message;
+
         this.close();
     }
 
--- Amex.CCA.WebClient/app/src/app/dashboard/dashboard.component.ts
+++ Amex.CCA.WebClient/app/src/app/dashboard/dashboard.component.ts
@@ -10,6 +10,7 @@ import { EditCreditCardComponent } from '../creditcard/editcreditcard.component'
 import { ApproveCreditCardComponent } from '../creditcard/approvecreditcard.component';
 import { DialogService } from "ng2-bootstrap-modal";
 
+
 @Component({
     templateUrl: './dashboard.template.html',
     styleUrls: ['./dashboard.styles.scss']
@@ -23,6 +24,7 @@ export class DashboardComponent {
     selectedIndex: number;
     cardRequestListTemp: ICreditCard[];
     promptMessage: string = '';
+    creditCard: ICreditCard = null;
     constructor(private actRouter: ActivatedRoute, private router: Router, private crediCardService: CrediCardService, private dialogService: DialogService) {
     }
 
@@ -30,7 +32,7 @@ export class DashboardComponent {
         this.crediCardService.getAllCardRequests().subscribe(receivedCardRequests => {
             this.cardRequestList = receivedCardRequests;
             this.cardRequestListTemp = this.cardRequestList;
-        this.selectedIndex = 0;
+            this.selectedIndex = 0;
             if (this.cardRequestList.length != 0) {
                 this.getFullNameFilterList(this.cardRequestList);
                 this.getDisplayNameFilterList(this.cardRequestList);
@@ -38,7 +40,7 @@ export class DashboardComponent {
                 this.getCardStatusFilterList(this.cardRequestList);
             }
         }, error => console.log(error));
-            
+
     }
 
     clickRequest(index: number) {
@@ -47,6 +49,7 @@ export class DashboardComponent {
 
     openCreditCardView(cardId: number) {
         this.dialogService.addDialog(ViewCreditCardComponent, {
+            CreditCard: this.creditCard,
             CreditCardId: cardId
         }).subscribe((message) => {
             //We get dialog result
@@ -56,6 +59,18 @@ export class DashboardComponent {
 
     openCreditCardEdit(cardId: number) {
         this.dialogService.addDialog(EditCreditCardComponent, {
+            CreditCard: this.creditCard,
+            CreditCardId: cardId
+        })
+        .subscribe((message) => {
+            //We get dialog result
+            this.promptMessage = message;
+        });
+    }
+
+    openCreditCardApprove(cardId: number) {
+        this.dialogService.addDialog(ApproveCreditCardComponent, {
+            CreditCard: this.creditCard,
             CreditCardId: cardId
         })
             .subscribe((message) => {
@@ -64,25 +79,15 @@ export class DashboardComponent {
             });
     }
 
-    openCreditCardApprove() {
-        this.dialogService.addDialog(ApproveCreditCardComponent, {
-            question: 'What is your name?: '
-        })
-            .subscribe((message) => {
-                //We get dialog result
-                this.promptMessage = message;
-            });
-    }
-
     getFullNameFilterList(cardRequestList: ICreditCard[]) {
-        this.fullNameList=[];
+        this.fullNameList = [];
         let k: number = this.fullNameList.push(cardRequestList[0].FullName);
         for (let i: number = k; i < cardRequestList.length; i++) {
             for (let j: number = 0; j < k; j++) {
                 if (this.fullNameList[j] === cardRequestList[i].FullName) {
-                    j = k;               
+                    j = k;
                 }
-                else if (j==(k-1)) {
+                else if (j == (k - 1)) {
                     k = this.fullNameList.push(cardRequestList[i].FullName)
                 }
             }
@@ -97,7 +102,7 @@ export class DashboardComponent {
                 if (this.displayNameList[j] === cardRequestList[i].DisplayName) {
                     j = k;
                 }
-                else if (j == (k-1)) {
+                else if (j == (k - 1)) {
                     k = this.displayNameList.push(cardRequestList[i].DisplayName)
                 }
             }
@@ -112,7 +117,7 @@ export class DashboardComponent {
                 if (this.cardTypeList[j] === cardRequestList[i].CardTypeName) {
                     j = k;
                 }
-                else if (j == (k-1)) {
+                else if (j == (k - 1)) {
                     k = this.cardTypeList.push(cardRequestList[i].CardTypeName)
                 }
             }
@@ -127,7 +132,7 @@ export class DashboardComponent {
                 if (this.cardStatusList[j] === cardRequestList[i].CardStatusName) {
                     j = k;
                 }
-                else if (j == (k-1)) {
+                else if (j == (k - 1)) {
                     k = this.cardStatusList.push(cardRequestList[i].CardStatusName)
                 }
             }
@@ -137,7 +142,7 @@ export class DashboardComponent {
     filterByCardType(cardType: string) {
         this.cardRequestList = this.cardRequestListTemp;
         let filtercardRequestList: ICreditCard[] = [];
-        if(this.cardRequestList.length != 0){
+        if (this.cardRequestList.length != 0) {
             for (let i: number = 0; i < this.cardRequestList.length; i++) {
                 if (this.cardRequestList[i].CardTypeName == cardType) {
                     let k = filtercardRequestList.push(this.cardRequestList[i]);
@@ -145,7 +150,7 @@ export class DashboardComponent {
             }
             this.cardRequestListTemp = this.cardRequestList;
             this.cardRequestList = filtercardRequestList;
-        }        
+        }
     }
 
     filterByFullName(fullName: string) {
