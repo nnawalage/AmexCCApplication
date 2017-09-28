318f40a2cf3dce51eedc30cd325f8fb352586a14
SL\msa <msa@tiqri.com>
2017-09-28 17:13:11 +0530

    uncommited data

--- Amex.CCA.WebClient/app/src/app/services/userprofile.service.ts
+++ Amex.CCA.WebClient/app/src/app/services/userprofile.service.ts
@@ -103,10 +103,10 @@ export class UserProfileService {
         return this.http
             .post(url, JSON.stringify(userData))
             .map((responce: Response) => responce.json())
-            .catch(this.handleError);
+            .catch(this.handleErrors);
 
     }
-    private handleError(error: any) {
+    private handleErrors(error: any) {
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
         console.error(errMsg);
