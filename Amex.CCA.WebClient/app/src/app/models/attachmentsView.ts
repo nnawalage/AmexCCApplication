9634f5b50a28302ec91523a86d50dcba7195fd27
Dhammika Herath <dhh@tiqri.com>
2017-09-29 09:54:41 +0530

    dashboard attachment showing

---
+++ Amex.CCA.WebClient/app/src/app/models/attachmentsView.ts
@@ -0,0 +1,7 @@
+export interface IAttachmentView {
+    AttachmentId :number,
+    FileUrl :string,
+    FileName :string,
+    //AttachmentType=Attachment.AttachmentType,
+    AttachmentTypeId :number
+}
